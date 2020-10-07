dc6a35ec5871224afaf05d402d083fa239e40eac
Oscar ChC Dell Laptop <osquidoors@hotmail.com>
2020-10-06 21:58:01 -0400

    login con los dos campos y el submit, ahora a crear el validar

--- login.php
+++ login.php
@@ -3,6 +3,10 @@
 <html>
 <title>Web</title>
 <body>
-Hola mundo
+<form action="./validar.php" method="get">
+Usuario: <input type="text" name="usuario" placeholder="usuario"/><br/><br/>
+Clave: <input type="password" name="clave" placeholder="clave"/><br/><br/>
+<input type="submit" value="Ingresar"/>
+</form>
 </body>
 </html>
