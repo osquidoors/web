9a797c425e0af73a72929de73c350e0fef3f772c
Oscar ChC Dell Laptop <osquidoors@hotmail.com>
2020-10-19 22:48:00 -0400

    lista los estudiantes de curso

--- estudiantesDeCurso.php
+++ estudiantesDeCurso.php
@@ -18,13 +18,42 @@ if ( isset( $_SESSION['user_id'] ) ) {      //isset función que verifica la exi
 <tr>
     <th>N&deg;</th>
     <th>Estudiante</th>
-    <th>N1</th>
-    <th>N2</th>
-    <th>N3</th>
-    <th>N4</th>
+    <th>Nota Tri1</th>
+    <th>Nota Tri2</th>
+    <th>Nota Tri3</th>
     <th>Promedio</th>
 </tr>
+<?php
+$conexion = mysqli_connect("localhost", "root", "", "mi_prueba");
+
+if(! $conexion ) {
+    echo "Error en la conexion a MySQL: ".mysqli_error();
+    die();
+}
+
+$_idCurso = $_GET['curso'];
+$sql = "SELECT * FROM estudiante WHERE Curso_idCurso='$_idCurso';";
+// recuperamos las cursos del docente logueado
+$resultado = mysqli_query($conexion, $sql);     //$resultado almacena todas las filas resultantes de la consulta
+$cont = 0;
+if ( !empty($resultado) && mysqli_num_rows($resultado) > 0 ) {
+    while($fila = mysqli_fetch_assoc($resultado)) {     //recorre todas las filas encontradas
+        $cont++;
+        $_identificador = $fila['rude'];
+        echo "<tr>";
+        echo "    <td>$cont</td>";
+        echo "    <td>".$fila["nombre_est"]." ".$fila["app_est"]." ".$fila["apm_est"]."</td>";
+        echo "    <td><input type='text' id='$_identificador-t1'></td>";
+        echo "    <td><input type='text' id='$_identificador-t2'></td>";
+        echo "    <td><input type='text' id='$_identificador-t3'></td>";
+        echo "    <td>Prom</td>";
+        echo "</tr>";
+    } // while
+} // if empty
+mysqli_close($conexion);
+?>
 </table>
+<input type="submit" value="Guardar notas"/>
 </form>
 </html>
 <?php
